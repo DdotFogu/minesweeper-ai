c852e4bbe136bfefb090a8715a6a4da390797098
John Hayden <fogu7825@gmail.com>
2026-04-02 19:31:58 -0400

    chore: removed runDemo from main

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './style/index.css';
 import App from './App.tsx';
 
-import {runDemo} from "./game/game.ts";
-
-runDemo();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
